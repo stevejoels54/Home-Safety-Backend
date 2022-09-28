496871da52b438dbe846fe02a5a8049c3f80ae5f
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2022-09-28 21:45:57 +0300

    Made debug false

--- home_safety_server/settings.py
+++ home_safety_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-==wf%+7k7bgoarb-phaglmeakq1*24r08an$k_=2&_%az&w2hw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.194.119",
                  "192.168.194.61", "home-safety-backend.herokuapp.com"]
